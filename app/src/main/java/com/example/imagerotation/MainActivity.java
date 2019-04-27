20e034be9be2c69e4380cdffa333568751b92ba7
LIUDINGYU <liudy.zju@gmail.com>
2019-04-27 14:12:54 +0800

    改变平移和旋转的模式

--- app/src/main/java/com/example/imagerotation/MainActivity.java
+++ app/src/main/java/com/example/imagerotation/MainActivity.java
@@ -16,14 +16,14 @@ public class MainActivity extends AppCompatActivity {
         Thread testThread = new Thread(new Runnable() {
             @Override
             public void run() {
-                for (int i = 0; i<12; i++){
+                for (int i = 0; i<100; i++){
                     try{
                         test.setPivotX(test.getWidth()/2);  //设置旋转中心
                         test.setPivotY(test.getHeight()*4/13);
-                        test.setTranslationX(i*15);           //设置边平移边旋转
-                        test.setTranslationY(i*15);
-                        test.setRotation(i*30);              //设置旋转角度，还有setRotationX() setRotationY()
-                        Thread.sleep(1000);
+                        test.setTranslationX(i*3);           //设置边平移边旋转
+                        test.setTranslationY(i*3);
+                        test.setRotation(i*5);              //设置旋转角度，还有setRotationX() setRotationY()
+                        Thread.sleep(50);            //如果刷新时间50ms
                     }catch (InterruptedException e){
                         e.printStackTrace();
                     }
